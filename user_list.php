42e2c00e0c4fc703d23a2e26bce4a033b34d7d1b
Rajnandan Kushwaha <rajnandanusa@gmail.com>
2023-08-28 13:10:33 +0530

    Create user_list.php

---
+++ user_list.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User List</title>
+</head>
+<body>
+    <h1>User List</h1>
+    
+    <?php
+    // Database connection
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'user_registration';
+    
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    // Fetch users from database
+    $sql = "SELECT id, name, email FROM users";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        echo "<ul>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<li><a href='edit_user.php?id={$row['id']}'>{$row['name']}</a> ({$row['email']})</li>";
+        }
+        echo "</ul>";
+    } else {
+        echo "No users found.";
+    }
+    
+    $conn->close();
+    ?>
+</body>
+</html>
