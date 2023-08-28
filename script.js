95b45a9e876d825dca6cfb2b6ad5afcdeb70c1d4
Rajnandan Kushwaha <rajnandanusa@gmail.com>
2023-08-28 13:02:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 document.getElementById('registration-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    // Perform client-side validation here
+    
+    // Clear previous error messages
+    clearErrors();
+
+    // Perform validation
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm_password').value;
+    const userImage = document.getElementById('user_image').value;
+    const captchaResponse = document.getElementById('g-recaptcha-response').value;
+
+    if (name === '') {
+        displayError('name', 'Name is required');
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        displayError('email', 'Invalid email format');
+        return;
+    }
+
+    if (password === '') {
+        displayError('password', 'Password is required');
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        displayError('confirm_password', 'Passwords do not match');
+        return;
+    }
+
+    if (userImage === '') {
+        displayError('user_image', 'User image is required');
+        return;
+    }
+
+    if (captchaResponse === '') {
+        displayError('captcha', 'Please complete the captcha');
+        return;
+    }
 
     // Submit the form data using AJAX
     const formData = new FormData(this);
@@ -17,3 +57,20 @@ document.getElementById('registration-form').addEventListener('submit', function
         console.error(error);
     });
 });
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function displayError(field, message) {
+    const errorElement = document.createElement('div');
+    errorElement.className = 'error-message';
+    errorElement.innerText = message;
+    document.getElementById(field + '-error').appendChild(errorElement);
+}
+
+function clearErrors() {
+    const errorElements = document.querySelectorAll('.error-message');
+    errorElements.forEach(element => element.remove());
+}
