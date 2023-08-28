d4787836f0204e1ab869af04998a44d983372754
Rajnandan Kushwaha <rajnandanusa@gmail.com>
2023-08-28 12:56:48 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_image VARCHAR(255) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
